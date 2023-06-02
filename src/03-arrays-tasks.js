15644ff6402eded50136751bfe517617fd5fe941
PopovaOlha <fc.odessa.10@gmail.com>
2023-06-02 16:37:50 +0300

    feat: add a solution of getArrayOfPositive task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -57,8 +57,10 @@ function generateOdds(len) {
  *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
  *    [] => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  const doubled = arr;
+  const second = arr;
+  return doubled.concat(second);
 }
 
 
@@ -73,8 +75,8 @@ function doubleArray(/* arr */) {
  *    [-1, 2, -5, -4, 0] => [ 2 ]
  *    [] => []
  */
-function getArrayOfPositives(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfPositives(arr) {
+  return arr.filter((elem) => elem > 0);
 }
 
 /**
