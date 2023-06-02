4513e5f00f09c906a227721a61dd829d2ed7e5f6
PopovaOlha <fc.odessa.10@gmail.com>
2023-06-02 15:51:29 +0300

    feat: add a solution by generateOdds task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -38,8 +38,10 @@ function findElement(arr, value) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return new Array(len)
+    .fill(0)
+    .map((elem, index) => index * 2 + 1);
 }
 
 
