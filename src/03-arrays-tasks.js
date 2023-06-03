8ac3bdbb4779b46fd13e56bc8d19e3e0d89081b4
PopovaOlha <fc.odessa.10@gmail.com>
2023-06-03 18:28:33 +0300

    feat: add a solution of the getPositivesCount task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -321,8 +321,8 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr.filter((v) => typeof v === 'number' && v > 0).length;
 }
 
 /**
