292ff7f723d69c8409c803d73c4792aba3c4c5a2
PopovaOlha <fc.odessa.10@gmail.com>
2023-06-09 22:11:28 +0300

    feat: add a solution by getFalsyValuesCount task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -371,8 +371,13 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((acc, elem) => {
+    if (!elem) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
 }
 
 /**
